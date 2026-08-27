026d63086696b9b9692c8b7f9fb66735225053d3
Tungsten842 <886724vf@anonaddy.me>
2026-08-27 19:32:34 +0200

    Cleanup

--- src/yolo.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from ultralytics import YOLO
-
-os.environ["YOLO_AUTOINSTALL"] = "false"
-model = YOLO("yolo11n.pt")
-
-# Export the model to ONNX format
-model.export(format="onnx", quantize=16, device=0, imgsz=(704, 1280), batch=2)
